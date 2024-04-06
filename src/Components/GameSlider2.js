29d4db4d8a64b55524d9131e4799db89883908f9
MUJAHID-WEB <mujahid30390@gmail.com>
2024-04-06 12:54:52 +0600

    slider fixed

--- src/Components/GameSlider2.js
+++ src/Components/GameSlider2.js
@@ -14,22 +14,22 @@ import {
   slide_image_5,
   slide_image_7,
 } from "../Imports/ImportImages";
+import "../App.css";
 
-import useWindowDimensions from "./WindowDimensions";
 
 // SwiperCore.use([EffectCoverflow, Pagination, Navigation,Autoplay]);
 
 function GameSlider2() {
-  const { width } = useWindowDimensions();
   var settings = {
     centerMode: true,
-    centerPadding: "15px",
-    // touchThreshold: 100,
+    centerPadding: "0",
+  //  touchThreshold: 5,
     dots: true,
     slidesToShow: 5,
     draggable: true,
     infinite: true,
     speed: 800,
+    swipeToSlide: true,
     // autoplay : true,
     responsive: [
       {
@@ -37,7 +37,7 @@ function GameSlider2() {
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "150px",
           slidesToShow: 3,
         },
       },
@@ -53,32 +53,23 @@ function GameSlider2() {
     ],
   };
 
+  const images = [slide_image_1, slide_image_2, slide_image_3, slide_image_4, slide_image_5, slide_image_7];
+
   return (
     <div className="gameSlider">
       <div className="container">
         <Slider {...settings}>
-          <div>
-            <img src={slide_image_1} />
-          </div>
-          <div>
-            <img src={slide_image_2} />
-          </div>
-          <div>
-            <img src={slide_image_3} />
-          </div>
-          <div>
-            <img src={slide_image_4} />
-          </div>
-          <div>
-            <img src={slide_image_5} />
-          </div>
-          <div>
-            <img src={slide_image_7} />
-          </div>
+          {images.map((image, index) => (
+            <div key={index} className="sliderItem">
+              <img src={image} alt={`slide-${index + 1}`} />
+            </div>
+          ))}
         </Slider>
       </div>
     </div>
   );
+
+
 }
 
 export default GameSlider2;
